d6f194333f6705de7c8126151d7a30bc6bdb1753
Maciek Wcislik <maciekwcislik@gmail.com>
2016-04-18 15:10:15 +0200

    Prepared response parsing for changes

--- src/waterlily_response.erl
+++ src/waterlily_response.erl
@@ -4,13 +4,40 @@
 
 -include("waterlily.hrl").
 
--define(NEWLINE, <<"\n">>).
+-define(REDIRECT,       <<"^">>).
+-define(ERROR,          <<"!">>).
+-define(PROMPT,         <<"">>).
+-define(QUERY,          <<"&">>).
+-define(SCHEMA_HEADER,  <<"%">>).
+-define(ASYNC_REPLY,    <<"_">>).
 
+-define(TABLE,          1).
+-define(UPDATE,         2).
+-define(CREATE,         3).
+-define(TRANSACTION,    4).
+-define(PREPARE,        5).
+-define(BLOCK,          6).
+
+
+% all of this needs to be handled correctly (and tested)
+% Redirect - ignore
+% error, log
+% prompt, ok, empty response
+% query, parse resposne
+% schema header, decide later
+% async_reply, TODO find out what is it
+%
+% header
+% TABLE or PREPARE - parse response
+% UPDATE - comes empty
+% CREATE -comes empty
+% TRANSACTION - as update
+% BLOCK - continuation for prev query
 decode(<<"&">>) ->
     <<>>;
 decode(<<"&", _/binary>> = Response) ->
     [Header, TableNames, ColumnNames, ColumnTypes, ColumnLengts | Rows] =
-        binary:split(Response, [?NEWLINE], [global]),
+        binary:split(Response, [<<$\n>>], [global]),
         ?DEBUG("ColumnTypes ~p~n", [ColumnTypes]),
         ?DEBUG("Rows ~p~n", [Rows]),
         ColumnTypes1 = column_types(ColumnTypes),
@@ -18,15 +45,26 @@ decode(<<"&", _/binary>> = Response) ->
         Rows2 = type_rows(Rows1, ColumnTypes1),
         Rows2;
 
-
 decode(Response) ->
     Response.
 
 
 column_types(ColumnTypes) ->
     [_, ColumnTypes1 | _] = binary:split(ColumnTypes, [<<" ">>], [global]),
-    binary:split(ColumnTypes1, [<<",\t">>], [global]).
+    CT = binary:split(ColumnTypes1, [<<",\t">>], [global]),
+    [to_atom(C) || C <- CT].
 
+
+to_atom(<<"boolean">>) -> boolean;
+to_atom(<<"tinyint">>) -> int;
+to_atom(<<"smallint">>) -> int;
+to_atom(<<"int">>) -> int;
+to_atom(<<"wrd">>) -> int;
+to_atom(<<"bigint">>) -> int;
+to_atom(<<"real">>) -> float;
+to_atom(<<"double">>) -> float;
+to_atom(<<"decimal">>) -> float;
+to_atom(_) -> binary.
 
 decode_rows(Rows) ->
     decode_rows(Rows, []).
@@ -103,30 +141,17 @@ type_row([R | Row] , [CT | ColumnTypes] , Normalized) ->
 
 type("NULL", _) ->
     null;
-type(R, <<"boolean">>) ->
+type(R, boolean) ->
     R =:= "true";
-type(R, <<"tinyint">>) ->
+type(R, int) ->
     list_to_integer(R);
-type(R, <<"smallint">>) ->
-    list_to_integer(R);
-type(R, <<"int">>) ->
-    list_to_integer(R);
-type(R, <<"wrd">>) ->
-    list_to_integer(R);
-type(R, <<"bigint">>) ->
-    list_to_integer(R);
-
-type(R, <<"real">>) ->
+type(R, float) ->
     list_to_float(R);
-type(R, <<"double">>) ->
-    list_to_float(R);
-type(R, <<"decimal">>) ->
-    list_to_float(R);
-type(R, _Type) ->
+type(R, binary) ->
     R.
 
 % <<"sSELECT * FROM todo;">>
-
+% type, query_id, rows, cols, index
 % <<"&1 5 1 3 1\n% sys.todo,\tsys.todo,\tsys.todo # table_name\n% id,\tdescription,\tdone # name\n% int,\tvarchar,\tboolean # type\n% 1,\t15,\t5 # length\n[ 1,\t\"zrobic porzadek\",\tfalse\t]\n">>
 
 
