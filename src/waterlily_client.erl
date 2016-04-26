9afa5504d841f48def646b8c3af5dd246927002d
Maciek Wcislik <maciekwcislik@gmail.com>
2016-04-26 14:12:02 +0200

    Added throttling

--- src/waterlily_client.erl
+++ src/waterlily_client.erl
@@ -26,14 +26,18 @@
 -include("waterlily.hrl").
 
 -record(state,
-            { host = "localhost" :: inet:host()
-            , port = 50000       :: inet:port()
-            , socket             :: undefined | inet:socket()
-            , keepalive = true   :: boolean()
-            , reconnect = true   :: boolean()
-            , message            :: #message{}
-            , data               :: binary()
-            , handler            :: atom()
+            { host = "localhost"       :: inet:host()
+            , port = 50000             :: inet:port()
+            , socket                   :: undefined | inet:socket()
+            , keepalive = true         :: boolean()
+            , reconnect = true         :: boolean()
+            , message                  :: #message{}
+            , data                     :: binary()
+            , handler                  :: atom()
+            , throttle_min = 0         :: non_neg_integer()
+            , throttle_now = 0         :: non_neg_integer()
+            , throttle_by = fun pow2/1 :: fun()
+            , throttle_max = 120       :: non_neg_integer()
             }).
 
 -define(TCP_OPTS, [binary, {active, false}, {packet, raw}]).
@@ -66,22 +70,29 @@ init([ResponseHandler]) ->
     end.
 
 try_connect(#state{host=Host, port=Port, keepalive=Keepalive,
-                   reconnect=Reconnect} = State) ->
+                   reconnect=Reconnect, throttle_now=TN, throttle_by=TF,
+                   throttle_min=TMIN, throttle_max=TMAX} = State) ->
     case gen_tcp:connect(Host, Port, [{keepalive, Keepalive} | ?TCP_OPTS]) of
       {ok, Socket} ->
           ?INFO("Connected to ~p:~p", [Host, Port]),
           ok = inet:setopts(Socket, [{active, once}]),
-          State2 = State#state{socket = Socket},
+          State2 = State#state{socket = Socket, throttle_now=TMIN},
           {ok, connected, State2};
       {error, Reason} ->
           ?ERROR("Cannot connect to ~p:~p: ~s", [Host, Port, Reason]),
-          case Reconnect of
+          State1 = case Reconnect of
               true ->
-                  gen_fsm:send_event_after(0, reconnect);
+                  NewThrottle = case TF(TN) > TMAX of
+                      true -> TMAX;
+                      _ -> TF(TN)
+                  end,
+                  ?DEBUG("Reconnecting after ~p~n", [NewThrottle]),
+                  gen_fsm:send_event_after(NewThrottle * 1000, reconnect),
+                  State#state{throttle_now=NewThrottle};
               false ->
-                  ok
+                  State
           end,
-          {ok, disconnected, State}
+          {ok, disconnected, State1}
     end.
 
 
@@ -150,7 +161,6 @@ ready(_Event, _From, State) ->
     {reply, Reply, ready, State}.
 
 
-
 handle_event(Event, StateName, State) ->
     ?DEBUG("Got some event ~p~n", [Event]),
     {next_state, StateName, State}.
@@ -210,3 +220,15 @@ send_message(Socket, Message) ->
 bin_to_hex(Bin) ->
     B = [io_lib:format("~2.16.0b", [N]) || N <- binary_to_list(Bin)],
     list_to_binary(B).
+
+add1(Throttle) ->
+    Throttle + 1.
+
+pow2(0) -> 1;
+pow2(Throttle) ->
+    Throttle * 2.
+
+exp(0) -> 2;
+exp(1) -> 2;
+exp(Throttle) ->
+    Throttle * Throttle.
