8202136a5f1d7ad5bd89f30f9bcd332ad4bd0b91
Maciek Wcislik <maciekwcislik@gmail.com>
2016-05-05 01:29:10 +0200

    Added tests for uuid, date and time statements.

--- src/waterlily_prepare.erl
+++ src/waterlily_prepare.erl
@@ -35,29 +35,24 @@ to_string(true) ->
 to_string(false) ->
     "false";
 to_string({uuid, UUID}) ->
-    to_string("uuid ", UUID);
+    "uuid " ++ to_string(UUID);
 to_string({date, Date}) ->
-    to_string("date ", Date);
+    "date " ++ to_string(Date);
 to_string({time, Time}) ->
-    to_string("time ", Time);
+    "time " ++ to_string(Time);
 to_string({timetz, TimeTZ}) ->
-    to_string("timetz ", TimeTZ);
+    "timetz " ++ to_string(TimeTZ);
 to_string({timestamp, Timestamp}) ->
-    to_string("timestamp ", Timestamp);
+    "timestamp " ++ to_string(Timestamp);
 to_string({timestamptz, TimestampTZ}) ->
-    to_string("timestamptz ", TimestampTZ);
+    "timestamptz " ++ to_string(TimestampTZ);
 to_string(Integer) when is_integer(Integer) ->
     integer_to_list(Integer);
 to_string(Float) when is_float(Float) ->
     float_to_list(Float);
 to_string(Bin) when is_binary(Bin) ->
-    [$', binary_to_list(quote(Bin)), $'];
+    quote(binary_to_list(Bin));
 to_string(List) when is_list(List) ->
     quote(List);
 to_string({json, JSON}) when is_binary(JSON)->
     "json '" ++ quote(binary_to_list(JSON)) ++ "'".
-
-to_string(Prefix, Data) when is_binary(Data) ->
-    Prefix ++ binary_to_list(Data);
-to_string(Prefix, Data) when is_list(Data) ->
-    Prefix ++ Data.
--- test/waterlily_db_SUITE.erl
+++ test/waterlily_db_SUITE.erl
@@ -1,7 +1,6 @@
 -module(waterlily_db_SUITE).
 
 -compile(export_all).
-
 -include_lib("common_test/include/ct.hrl").
 
 
@@ -12,6 +11,8 @@ all() ->
   , t_update_row
   , t_delete_row
   , t_prepare_statement
+  , t_basic_uuid_date_time
+  , t_prepared_uuid_date_time
   ].
 
 init_per_testcase(t_create_db, Config) ->
@@ -27,20 +28,31 @@ init_per_testcase(t_create_db, Config) ->
     end,
     [{pid, Pid} | Config];
 
-init_per_testcase(_, Config) ->
-    application:load(waterlily),
-    {ok, Pid} = waterlily:connect(),
-    Query = <<"SELECT name FROM tables WHERE name='waterlily_test';">>,
-    {_,_,R} = wait_for_query(Pid, 5, Query),
-    ok = case R of
-        [] ->
+init_per_testcase(t_basic_uuid_date_time, Config) ->
+    Q = <<"CREATE TABLE waterlily_test
+           (id UUID,
+            wl_date DATE,
+            wl_time TIME,
+            wl_timetz TIME WITH TIME ZONE,
+            wl_timestamp TIMESTAMP,
+            wl_timestamptz TIMESTAMP WITH TIME ZONE);">>,
+    Pid = init_db(Q),
+    [{pid, Pid} | Config];
 
-            waterlily:query(Pid,
-                <<"CREATE TABLE waterlily_test (id INT, description
-                  VARCHAR(255));">>);
-        _ ->
-            ok
-    end,
+init_per_testcase(t_prepared_uuid_date_time, Config) ->
+    Q = <<"CREATE TABLE waterlily_test
+           (id UUID,
+            wl_date DATE,
+            wl_time TIME,
+            wl_timetz TIME WITH TIME ZONE,
+            wl_timestamp TIMESTAMP,
+            wl_timestamptz TIMESTAMP WITH TIME ZONE);">>,
+    Pid = init_db(Q),
+    [{pid, Pid} | Config];
+
+init_per_testcase(_, Config) ->
+    Pid = init_db(<<"CREATE TABLE waterlily_test (id INT, description
+                VARCHAR(255));">>),
     [{pid, Pid} | Config].
 
 end_per_testcase(_, Config) ->
@@ -99,7 +111,62 @@ t_prepare_statement(Config) ->
     {_, _, [[10]]} 
         = waterlily:query(Pid, <<"SELECT COUNT(*) FROM waterlily_test;">>).
 
+t_basic_uuid_date_time(Config) ->
+    Pid = ?config(pid, Config),
+    {ok, _} = waterlily:query(Pid,
+        <<"INSERT INTO waterlily_test VALUES(uuid(), current_date(),
+        current_time(), current_time(), current_timestamp(),
+        current_timestamp());">>),
+    {_,_,[Row]} = waterlily:query(Pid, <<"SELECT * FROM waterlily_test;">>),
+    [{uuid, _}
+    ,{date, _}
+    ,{time, _}
+    ,{timetz, _}
+    ,{timestamp, _}
+    ,{timestamptz, _}] = Row.
+
+t_prepared_uuid_date_time(Config) ->
+    Pid = ?config(pid, Config),
+    Response = waterlily:prepare(Pid,
+        <<"INSERT INTO waterlily_test VALUES(?,?,?,?,?,?);">>),
+    QueryId = waterlily:query_id(Response),
+    UUID = "c450f9f6-e50c-4e57-9116-4e591512a1b1",
+    Date = "2016-05-04",
+    Time = "22:12:55",
+    Timetz = "22:12:54+00:00",
+    Timestamp = "2016-05-04 22:11:53.123000",
+    Timestamptz = "2016-05-03 21:10:52.012000+00:00",
+    {ok,_} = waterlily:execute(Pid, QueryId,
+                               [{uuid, UUID}
+                               ,{date, Date}
+                               ,{time, Time}
+                               ,{timetz, Timetz}
+                               ,{timestamp, Timestamp}
+                               ,{timestamptz, Timestamptz}]),
+    {_,_,[Row]} = waterlily:query(Pid, <<"SELECT * FROM waterlily_test;">>),
+    [{uuid, UUID}
+    ,{date, Date}
+    ,{time, Time}
+    ,{timetz, Timetz}
+    ,{timestamp, Timestamp}
+    ,{timestamptz, Timestamptz}] = Row.
+    
+
 % helpers
+
+init_db(Q) ->
+    application:load(waterlily),
+    {ok, Pid} = waterlily:connect(),
+    Query = <<"SELECT name FROM tables WHERE name='waterlily_test';">>,
+    {_,_,R} = wait_for_query(Pid, 5, Query),
+    ok = case R of
+        [] ->
+            waterlily:query(Pid, Q);
+        _ ->
+            ok
+    end,
+    Pid.
+
 wait_for_query(_Pid, 0, _Q) ->
     error;
 wait_for_query(Pid, N, Q) ->
